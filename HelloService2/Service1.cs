0331c188bfc60043a8defa5cfe22fdea2ff08914
Senthil <sentabe@gmail.com>
2019-04-17 07:31:31 -0700

    changes in getdata parmaeter value

--- HelloService2/Service1.cs
+++ HelloService2/Service1.cs
@@ -10,9 +10,9 @@ namespace HelloService2
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string GetData(int value1)
         {
-            return string.Format("You entered: {0}", value);
+            return string.Format("You entered: {0}", value1);
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
